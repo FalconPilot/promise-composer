79d6049f76cd26cd98b7dcc3479e8c4ed6cddefc
Raphael Pallies <raphael@equisense.fr>
2018-09-19 18:25:01 +0200

    0.3.3 : refactored test file, added PCO.any/PCO.all functions

--- src/promise-composer.js
+++ src/promise-composer.js
@@ -4,24 +4,66 @@
 **  Promise Composer Object class
 */
 
-const ASSERTION_FALSE = "ASSERTION_FALSE"
-
 class PCO {
 
+  /*
+  **  Class constants
+  */
+
+  static get ASSERTION_FALSE() { return "PCO_ASSERTION_FALSE" }
+
+  /*
+  **  Common functions
+  */
+
   // Local assertion handling function
-  static promise(value, assertion) {
-    return assertion ? Promise.resolve(value) : Promise.reject(ASSERTION_FALSE)
+  static result(value, assertion) {
+    return assertion ? Promise.resolve(value) : Promise.reject(PCO.ASSERTION_FALSE)
   }
 
-  // Local method to simply return assertion, used for internal functionality
-  static cond(x, assertion) { return assertion }
+  // Muted assertion handling function (doesn't reject, only resolve)
+  static mutedPromise(value, assertion) {
+    return assertion ? Promise.resolve(value) : Promise.resolve(PCO.ASSERTION_FALSE)
+  }
+
+  // Local method to return pure value of assertion
+  static pure(x, assertion) { return assertion }
 
   /*
   **  Make assertion functions customizable
   */
 
-  static assert(x, assertion, func = PCO.promise) {
-    return func(x, assertion)
+  static assert(x, assertFunc, func = PCO.result) {
+    return func(x, assertFunc(x))
+  }
+
+  /*
+  **  Functions to allow more complex conditional composition
+  */
+
+  // General multiple assertion helper
+  static multiAssert(x, assertions, verif) {
+    return Promise.all(assertions.map(a => a(x, PCO.mutedPromise)))
+      .then(raw => {
+        const values = raw.filter(v => v !== PCO.ASSERTION_FALSE)
+
+        // Throw a .catch() clause if the verification function is false
+        if (verif(raw, values)) {
+          return x
+        } else {
+          throw PCO.ASSERTION_FALSE
+        }
+      })
+  }
+
+  // Continue if all conditions are true
+  static all(x, assertions) {
+    return PCO.multiAssert(x, assertions, (r, v) => v.length === r.length)
+  }
+
+  // Continue if one condition at least is true
+  static any(x, assertions) {
+    return PCO.multiAssert(x, assertions, (r, v) => v.length > 0)
   }
 
   /*
@@ -30,42 +72,42 @@ class PCO {
   */
 
   // Assert element is not undefined
-  static isset(x, func = PCO.promise) {
+  static isset(x, func = PCO.result) {
     return func(x, typeof x !== "undefined")
   }
 
   // Assert element is neither undefined or null
-  static exists(x, func = PCO.promise) {
-    return func(x, PCO.isset(x, PCO.cond)
+  static exists(x, func = PCO.result) {
+    return func(x, PCO.isset(x, PCO.pure)
       && x !== null
     )
   }
 
   // Assert element is a non-empty string
-  static fullString(x, func = PCO.promise) {
-    return func(x, PCO.exists(x, PCO.cond)
+  static fullString(x, func = PCO.result) {
+    return func(x, PCO.exists(x, PCO.pure)
       && typeof x === "string"
       && x.trim() !== ""
     )
   }
 
   // Assert element is valid non-array object
-  static isObject(x, func = PCO.promise) {
+  static isObject(x, func = PCO.result) {
     return func(x, typeof x === "object" && !Array.isArray(x))
   }
 
   // Assert element is valid array
-  static isArray(x, func = PCO.promise) {
+  static isArray(x, func = PCO.result) {
     return func(x, Array.isArray(x))
   }
 
   // Assert element is a valid number
-  static isNumber(x, func = PCO.promise) {
+  static isNumber(x, func = PCO.result) {
     return func(x, typeof x === "number")
   }
 
   // Assert element is an Integer
-  static isInteger(x, func = PCO.promise) {
+  static isInteger(x, func = PCO.result) {
     return func(x, Number.isInteger(x))
   }
 
--- tests/test.js
+++ tests/test.js
@@ -4,46 +4,63 @@ const PCO = require('../src/promise-composer.js')
 **  PCO unit tests
 */
 
-// Important constants
-const ASSERTION_FALSE = "ASSERTION_FALSE"
-
 // Shell colors
 const RED = "\033[31m"
 const GRE = "\033[32m"
 const YEL = "\033[33m"
+const MAG = "\033[35m"
 const NC  = "\033[0m"
 
-// Test helper
-function test(func, value, assertion = value) {
+// Render separator
+function renderSeparator(text, pre = '\n') {
+  console.log(`${pre}${MAG}--- ${text} ---${NC}\n`)
+}
+
+// Wrap variable
+function wrap(value) {
+  return Array.isArray(value)
+    ? `[${value}]`
+    : {
+        "object": `{${value}}`,
+        "string": `"${value}"`
+      }[typeof value]
+}
+
+// Simple test helper
+function test(func, value, assertion = value, args = [value]) {
   return new Promise((resolve, reject) => {
     Promise.resolve(value)
-      .then(func)
-      .then(resp => handleResponse(func, value, resp, assertion, resolve, reject))
-      .catch(err => handleResponse(func, value, err, assertion, resolve, reject))
-      .catch(console.log)
+      .then(x => func.apply(null, args))
+      .then(resp => handleResponse(func, value, resp, assertion, resolve))
+      .catch(resp => handleResponse(func, value, resp, assertion, resolve))
   }).catch(console.log)
+}
+
+// Test array functions
+function testArray(func, args, value, assertion = value) {
+  return test(func, value, assertion, [value].concat([args]))
 }
 
 // Function response handler
 function handleResponse(func, input, value, assertion, resolve) {
 
-  // Define constants
+  // Format string elements
   const funcName = func.name !== undefined ? func.name : "(lambda)"
   const result = value === assertion ? `${GRE}OK${NC}` : `${RED}KO${NC}`
-  const ast = typeof value === "string" ? `"${assertion}"` : assertion
-  const val = typeof value === "string" ? `"${value}"` : value
-  const inp = typeof input === "string" ? `"${input}"` : input
   const color = value === assertion ? GRE : RED
   const prefix = PCO.hasOwnProperty(funcName) ? `${YEL}PCO${NC}.` : ""
 
-  // Log results
-  console.log(`--> [${result}] Testing ${prefix}${funcName} using ${inp}`)
-  console.log(`|___ Expecting ${ast}`)
-  console.log(`|___ Obtained  ${val}\n`)
+  if (!process.argv.includes("--mute") || value !== assertion) {
+    // Log results
+    console.log(`--> [${result}] Testing ${prefix}${funcName} using ${wrap(input)}`)
+    console.log(`|___ Expecting ${wrap(assertion)}`)
+    console.log(`|___ Obtained  ${wrap(value)}\n`)
+  }
 
+  // Resolve parent promise with function/score values
   resolve({
     score: value === assertion ? 1 : 0,
-    func: funcName
+    func: `${prefix}${funcName}`
   })
 }
 
@@ -51,50 +68,68 @@ function handleResponse(func, input, value, assertion, resolve) {
 **  Play unit tests
 */
 
-console.log(`${YEL}--- Starting tests... ---${NC}\n`)
+const timestamp = new Date().getTime()
+renderSeparator(`Starting tests, timestamp ${NC}${timestamp}${MAG}`, '')
+
+if (process.argv.includes("--mute")) {
+  console.log(`/!\\ ${GRE}Tests are run in mute mode ${NC}[--mute]${GRE}, so only failed tests will appear below.\n${NC}`)
+}
 
 // Test custom assertion
-const customAssert = (x) => PCO.assert(x, Number.isInteger)
-
+const customAssert = (x) => PCO.assert(x, (arr) => Array.isArray(arr) && arr.length > 3)
 
 // Unit tests
 Promise.all([
 
   // PCO.isset
-  test(PCO.isset, undefined, ASSERTION_FALSE),
+  test(PCO.isset, undefined, PCO.ASSERTION_FALSE),
   test(PCO.isset, "Value"),
   test(PCO.isset, null),
 
   // PCO.exists
-  test(PCO.exists, null, ASSERTION_FALSE),
-  test(PCO.exists, undefined, ASSERTION_FALSE),
+  test(PCO.exists, null, PCO.ASSERTION_FALSE),
+  test(PCO.exists, undefined, PCO.ASSERTION_FALSE),
   test(PCO.exists, "", ""),
 
   // PCO.fullString
-  test(PCO.fullString, null, ASSERTION_FALSE),
-  test(PCO.fullString, undefined, ASSERTION_FALSE),
-  test(PCO.fullString, " ", ASSERTION_FALSE),
+  test(PCO.fullString, null, PCO.ASSERTION_FALSE),
+  test(PCO.fullString, undefined, PCO.ASSERTION_FALSE),
+  test(PCO.fullString, " ", PCO.ASSERTION_FALSE),
   test(PCO.fullString, "Coucou"),
-  test(customAssert, 15),
-  test(customAssert, "yeah", ASSERTION_FALSE)
+
+  // PCO.assert
+  test(customAssert, [1, 2, 3, 4]),
+  test(customAssert, [1, 2, 3], PCO.ASSERTION_FALSE),
+  test(customAssert, "yeah", PCO.ASSERTION_FALSE),
+
+  // PCO.any
+  testArray(PCO.any, [PCO.fullString, PCO.isInteger], "test"),
+  testArray(PCO.any, [PCO.fullString, PCO.isInteger], 15.5, PCO.ASSERTION_FALSE),
+  testArray(PCO.any, [PCO.fullString, PCO.exists], ""),
+
+  // PCO.all
+  testArray(PCO.all, [PCO.fullString, PCO.exists], "", PCO.ASSERTION_FALSE)
 
 ]).then(values => {
 
-  // Compute score
+  // Compute score and color
   const score = values.reduce((t, x) => (t + x.score), 0)
   const color = score > 0
     ? ( score < values.length
       ? YEL
       : GRE
-    )
-    : RED
+    ) : RED
+
+  // Define faulty functions to display
   const faulty = values.filter(x => x.score === 0)
     .map(x => x.func)
     .filter((x, pos, arr) => arr.indexOf(x) === pos)
     .map(x => `${RED}X${NC} ${x}`)
 
-  // Log final report
-  console.log("Tests done !")
+  // Log final score/fault report
+  renderSeparator("Tests completed !", '')
   console.log(`Final score : ${color}${score}/${values.length}${NC}`)
+  if (faulty.length > 0) { renderSeparator("Faulty functions list") }
   faulty.forEach(x => console.log(x))
+
 }).catch(console.log)
